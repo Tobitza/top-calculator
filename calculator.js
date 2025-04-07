312cc11428caae484debfe439bc51873e5f3cbaa
tubusor <160207643+Tobitza@users.noreply.github.com>
2025-04-08 00:04:01 +0300

    Added basic math operation logic

--- calculator.js
+++ calculator.js
@@ -1,63 +1,64 @@
 const display = document.querySelector("#calculator-display");
 const buttons = document.querySelectorAll(".calculator-button");
 
+// Var displayed in display in the format: VAR1, OPERATOR, VAR2
+let operand1 = null;
+let operand2 = null;
+let operator = null;
+
 // Input fetching
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        display.textContent = "";
         buttonTester(button);
-        // const value = button.textContent;
-        // display.textContent += value;
     });
 });
 
-// Objects for operators and special cases (C & =)
+// Objects for operators
 const operators = {
     "+": (a, b) => a + b,
     "-": (a, b) => a - b,
     "X": (a, b) => a * b,
-    "/": (a, b) => a / b,
-}
+    "/": (a, b) => a / b
+};
 
-const special = {
-    "C": clearDisplay(),
-    "=": calculate(),
-}
-
-
-
-// Operator-specific logic (non-numbers)
+// Logic based on the input
 function buttonTester (button) {
-    if (/[0-9]/.test(button.textContent)) {
-        display.textContent += button.textContent;
+    value = button.textContent;
+    if (/[0-9]/.test(value)) {
+        display.textContent += value;
     }
-    else {
-        
+    else if (value === ".") {
+        if (!display.textContent.includes(".")) {
+            display.textContent += value;
+        }
     }
-}
+    else if (value in operators) {
+        operand1 = display.textContent;
+        operator = value;
+        display.textContent = "";
+        console.log(operand1);
+    }
+    else if (value === "=") {
+        operand2 = display.textContent;
+        display.textContent = operate(operand1, operator, operand2);
+    }
+    else if (value === "C") {
+        clear();
+    }
+};
 
+function clear () {
+    operand1 = null;
+    operand2 = null;
+    operator = null;
+    display.textContent = "";
+};
 
+function operate (op1, operator, op2) {
+    const result = operators[operator](parseFloat(op1), parseFloat(op2));
+    return result;
+};
 
-
-
-// Var displayed in display in the format: VAR1, OPERATOR, VAR2
-let var1 = 0;
-let var2 = 0;
-let operator = "";
-
-// Basic calculations -DISCONTINUED, will use functions inside operators object (found above)
-// function add(a, b) {
-//     return a + b;
-// }
-
-// function subtract(a, b) {
-//     return a - b;
-// }
-
-// function multiply(a, b) {
-//     return a * b;
-// }
-
-// function divide(a, b) {
-//     return a / b;
-// }
+// TODO:
+// 1. Daisy-chaining operations by storing curr val & operator
+// 2. Disable operator buttons when one has already been pressed (no 2+ operators in a row)
